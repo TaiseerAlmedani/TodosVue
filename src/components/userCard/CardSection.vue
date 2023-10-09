85e442b3faec0547c7191f5ce5613eb96ad370c6
TAISEER ALMEDANI <taiseer.almedani@gmail.com>
2023-10-09 17:13:22 +0300

    fetch user data from api

--- src/components/userCard/CardSection.vue
+++ src/components/userCard/CardSection.vue
@@ -1,8 +1,17 @@
-
 <script>
 import CardItem from './CardItem.vue';
 export default {
   components: { CardItem },
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(usersArray => this.users = usersArray)
+  }
 }
 </script>
 <template>
@@ -11,15 +20,16 @@ export default {
       <h1>Our Team</h1>
     </div>
     <div class="row">
-      <card-item linkedin="https//www.linkedin.com" twitter="https//www.twitter.com">
-        <template #fullName>fathi</template>
-        <template #userName>fathi31234</template>
-        <template #email>fathi@mail.com</template>
-      </card-item>
-      <card-item linkedin="https//www.linkedin.com" twitter="https//www.twitter.com">
-        <template #fullName>fathia</template>
-        <template #userName>fathia31234</template>
-        <template #email>fathia@mail.com</template>
+      <card-item v-for="user in users" :key="user.id" :twitter="user.name" :linkedin="user.email">
+        <template #fullName>
+          {{ user.name }}
+        </template>
+        <template #userName>
+          {{ user.username }}
+        </template>
+        <template #email>
+          {{ user.email }}
+        </template>
       </card-item>
     </div>
   </section>
