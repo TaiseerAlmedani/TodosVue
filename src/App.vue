68ba3047e33b7ac9ea13131c37cc6b098199806e
TAISEER ALMEDANI <taiseer.almedani@gmail.com>
2023-10-10 05:53:55 +0300

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@ export default {
 </script>
 
 <template>
-  <card-section></card-section>
-  <todos-section />
+  <header class="header">
+    <a href="/"> click</a>
+    <a href="/about"> click</a>
+  </header>
+   <router-link to="/">Go to Home</router-link>
+   <router-link to="/about">Go to About</router-link>
+
+  <router-view></router-view>
+  
 </template>
+<style>
+a{
+  color: black !important;
+  font-size: 20px;
+  margin-right: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import './assets/main.css'
+import TodosSection from "./components/TodosSection.vue"
+import CardSection from "./components/userCard/CardSection.vue"
 
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: TodosSection },
+  { path: '/about', component: CardSection },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
